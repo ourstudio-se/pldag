22a89cc858b53986bfe76e3fbdde658ccb0ea83b
znittzel <rikard@ourstudio.se>
2024-04-12 09:24:55 +0200

    Alias concept removed

--- pldag/__init__.py
+++ pldag/__init__.py
@@ -27,8 +27,6 @@ class PLDAG:
         self._cvec = np.zeros((0, ),    dtype=bool)
         # Maps id's to index
         self._imap = {}
-        # Maps alias to id's
-        self._amap = {}
 
     @property
     def bounds(self) -> np.ndarray:
@@ -36,9 +34,9 @@ class PLDAG:
         return self._dvec
     
     @property
-    def aliases(self) -> List[str]:
-        """Get all aliases"""
-        return list(self._amap.keys())
+    def ids(self) -> List[str]:
+        """Get all ids"""
+        return list(self._imap.keys())
     
     @property
     def _toposort(self) -> iter:
@@ -51,109 +49,93 @@ class PLDAG:
             )
         ).static_order()
     
-    @property
-    def _rev_amap(self) -> dict:
-        # Reversing the dictionary
-        reversed_dict = {}
-        for key, value in self._amap.items():
-            reversed_dict.setdefault(value, set()).add(key)
-        return reversed_dict
+    def _icol(self, id: str) -> int:
+        """
+            Returns the column index of the given ID.
+        """
+        return self._imap[id]
     
-    def _icalias(self, alias: str) -> int:
+    def _irow(self, id: str) -> int:
         """
-            Returns the column index of the given alias or ID.
+            Returns the row index of the given ID.
         """
-        return self._imap[self._amap.get(alias, alias)]
+        return self._icol(id) - (~self._cvec).sum()
     
-    def _iralias(self, alias: str) -> int:
-        """
-            Returns the row index of the given alias or ID.
-        """
-        return self._icalias(alias) - (~self._cvec).sum()
+    def get(self, *id: str) -> np.ndarray:
+        """Get the bounds of the given ID(s)"""
+        return self._dvec[list(map(self._icol, id))]
     
-    def get(self, *alias: str) -> np.ndarray:
-        """Get the bounds of the given alias"""
-        return self._dvec[list(map(self._icalias, alias))]
+    def exists(self, id: str) -> bool:
+        """Check if the given id exists"""
+        return (id in self._imap)
     
-    def exists(self, alias: str) -> bool:
-        """Check if the given alias exists"""
-        return (alias in self._amap) or (alias in self._imap)
-    
-    def dependencies(self, alias: str) -> Dict[str, Set[str]]:
+    def dependencies(self, id: str) -> Set[str]:
         """
-            Get the dependencies of the given alias or ID.
-            A mapping of ID -> {Alias} is returned, where each ID
-            is a dependency to `alias`.
+            Get the dependencies of the given ID.
         """
-        return dict(
+        return set(
             map(
-                lambda x: (x, self._rev_amap.get(x, set())),
-                map(
-                    lambda x: list(self._imap)[x],
-                    np.argwhere(self._amat[self._iralias(alias)] == 1).T[0]
-                )
+                lambda x: list(self._imap)[x],
+                np.argwhere(self._amat[self._irow(id)] == 1).T[0]
             )
         )
     
-    def negated(self, alias: str) -> bool:
-        """Get the negated state of the given alias"""
-        return bool(self._nvec[self._iralias(alias)])
+    def negated(self, id: str) -> bool:
+        """Get the negated state of the given id"""
+        return bool(self._nvec[self._irow(id)])
     
-    def delete(self, alias: str) -> None:
-        """Delete the given alias"""
-        idx = self._icalias(alias)
+    def delete(self, id: str) -> None:
+        """Delete the given id"""
+        idx = self._icol(id)
 
         # Remove the primitive prime for all composite primes
         # May be changed later. Now we just set everything to 0
         # but keep the rows and columns
         self._amat[:, idx] = 0
     
-    def set_primitive(self, alias: str, bound: complex = complex(0,1)) -> str:
+    def set_primitive(self, id: str, bound: complex = complex(0,1)) -> str:
         """Add a primitive prime factor matrix"""
-        if alias in self._imap:
+        if id in self._imap:
             # Update value if already in map
-            self._dvec[self._icalias(alias)] = bound
+            self._dvec[self._icol(id)] = bound
         else:
             self._amat = np.hstack((self._amat, np.zeros((self._amat.shape[0], 1), dtype=np.uint8)))
             self._dvec = np.append(self._dvec, bound)
             self._cvec = np.append(self._cvec, False)
-            self._imap[alias] = self._amat.shape[1] - 1
+            self._imap[id] = self._amat.shape[1] - 1
 
-        return alias
+        return id
 
-    def set_primitives(self, aliases: List[str], bound: complex = complex(0,1)) -> List[str]:
+    def set_primitives(self, ids: List[str], bound: complex = complex(0,1)) -> List[str]:
         """Add multiple primitive prime factor matrices"""
-        for alias in aliases:
-            self.set_primitive(alias, bound)
-        return aliases
+        for id in ids:
+            self.set_primitive(id, bound)
+        return ids
 
-    def _set_gelineq(self, children: list, bias: int, negate: bool = False, alias: Optional[str] = None, force_id: Optional[str] = None) -> str:
+    def _set_gelineq(self, children: list, bias: int, negate: bool = False) -> str:
         """
             Add a composite constraint of at least `value`.
         """
         _bias = complex(*repeat(bias * (1-negate) + (bias + 1) * negate * -1, 2))
-        _id = sha1(("".join(sorted(children)) + str(negate)).encode()).hexdigest() if force_id is None else force_id
+        _id = sha1(("".join(sorted(children)) + str(negate)).encode()).hexdigest()
         if _id in self._imap:
             arr = np.zeros(self._amat.shape[1], dtype=np.int8)
             arr[[self._imap[child] for child in children]] = 1
-            self._amat[self._iralias(_id)] = arr
-            self._bvec[self._iralias(_id)] = _bias
-            self._nvec[self._iralias(_id)] = negate
+            self._amat[self._irow(_id)] = arr
+            self._bvec[self._irow(_id)] = _bias
+            self._nvec[self._irow(_id)] = negate
             self._dvec[self._imap[_id]] = complex(0, 1)
             self._cvec[self._imap[_id]] = True
         else:
             self._amat = np.pad(self._amat, ((0, 1), (0, 1)), mode='constant')
             arr = np.zeros(self._amat.shape[1], dtype=np.int8)
-            arr[[self._icalias(child) for child in children]] = 1
+            arr[[self._icol(child) for child in children]] = 1
             self._amat[self._amat.shape[0] - 1] = arr
             self._dvec = np.append(self._dvec, complex(0, 1))
             self._bvec = np.append(self._bvec, _bias)
             self._nvec = np.append(self._nvec, negate)
             self._cvec = np.append(self._cvec, True)
             self._imap[_id] = self._amat.shape[1] - 1
-        
-        if alias is not None:
-            self._amap[alias] = _id
 
         return _id
     
@@ -205,15 +187,15 @@ class PLDAG:
 
         # Propagate the graph and store the result as new bounds
         self._dvec = self._prop_algo(A, B, C, D, F, np.zeros(D.shape[0], dtype=bool))
-    
-    def _test(self, query: Dict[str, complex]) -> np.ndarray:
-        
+
+    def test(self, query: Dict[str, complex]) -> Dict[str, complex]:
+
         """
-            Propagates the graph and returns the propagated bounds.
+            Propagates the graph and returns the selected result.
 
-            query: what nodes and their bound to start propagating from
+            query:  what nodes and their bound to start propagating from
 
-            Returns the propagated bounds.
+            Returns the selected propagated bounds.
         """
         A: np.ndarray = self._amat
         B: np.ndarray = self._bvec
@@ -228,50 +210,39 @@ class PLDAG:
         # Replace the observed bounds
         D[qprimes] = np.array(list(query.values()))
 
-        return self._prop_algo(A, B, C, D, F, qprimes)
-
-    def test(self, query: Dict[str, complex]) -> Dict[str, complex]:
-
-        """
-            Propagates the graph and returns the selected result.
-
-            query:  what nodes and their bound to start propagating from
-
-            Returns the selected propagated bounds.
-        """
-        return dict(zip(self._imap.keys(), self._test(query)))
+        return dict(zip(self._imap.keys(), self._prop_algo(A, B, C, D, F, qprimes)))
     
-    def set_atleast(self, children: list, value: int, alias: Optional[str] = None, force_id: Optional[str] = None) -> str:
-        return self._set_gelineq(children, -1 * value, False, alias, force_id)
+    def set_atleast(self, children: list, value: int) -> str:
+        return self._set_gelineq(children, -1 * value, False)
     
-    def set_atmost(self, children: list, value: int, alias: Optional[str] = None, force_id: Optional[str] = None) -> str:
-        return self._set_gelineq(children, -1 * (value + 1), True, alias, force_id)
+    def set_atmost(self, children: list, value: int) -> str:
+        return self._set_gelineq(children, -1 * (value + 1), True)
     
-    def set_or(self, references: List[str], alias: Optional[str] = None) -> str:
-        return self.set_atleast(references, 1, alias=alias)
+    def set_or(self, references: List[str]) -> str:
+        return self.set_atleast(references, 1)
     
-    def set_and(self, references: List[str], alias: Optional[str] = None) -> str:
-        return self.set_atleast(references, len(references), alias=alias)
+    def set_and(self, references: List[str]) -> str:
+        return self.set_atleast(references, len(references))
     
-    def set_not(self, references: List[str], alias: Optional[str] = None) -> str:
-        return self.set_atmost(references, 0, alias=alias)
+    def set_not(self, references: List[str]) -> str:
+        return self.set_atmost(references, 0)
     
-    def set_xor(self, references: List[str], alias: Optional[str] = None) -> str:
+    def set_xor(self, references: List[str]) -> str:
         return self.set_and([
             self.set_atleast(references, 1),
             self.set_atmost(references, 1),
-        ], alias=alias)
+        ])
     
-    def set_imply(self, condition: str, consequence: str, alias: Optional[str] = None) -> str:
-        return self.set_or([self.set_not([condition]), consequence], alias=alias)
+    def set_imply(self, condition: str, consequence: str) -> str:
+        return self.set_or([self.set_not([condition]), consequence])
 
     def to_polyhedron(self, fix: dict = {}) -> tuple:
 
         """
             Returns a polyhedron of a tuple (A, b, variables) representation of the PL-DAG.
-            The variables list consists of tuples of (ID, alias, bound as complex number).
+            The variables list consists of tuples of (ID, bound as complex number).
 
-            fix: dict of aliases to fix in the polyhedron. E.g. {"A": 1, "B": 0} fix A=1 and B=0.
+            fix: dict of ids to fix in the polyhedron. E.g. {"A": 1, "B": 0} fix A=1 and B=0.
         """
 
         # Create the matrix
@@ -309,7 +280,7 @@ class PLDAG:
         for i, vs in groupby(zip(fix.values(), fix.keys()), key=lambda x: x[0]):
             v = list(map(lambda x: x[1], vs))
             a = np.zeros(A.shape[1], dtype=np.int64)
-            a[np.array(list(map(self._icalias, v)))] = 1
+            a[np.array(list(map(self._icol, v)))] = 1
             if i > 0 or i < 0:
                 _b = a.sum() * i
             else:
@@ -327,7 +298,6 @@ class PLDAG:
                 map(
                     lambda i: (
                         rimap[i],
-                        self._rev_amap.get(rimap[i], {}),
                         self._dvec[i],
                     ),
                     np.array(list(self._imap.values()))
--- tests/test_pldag.py
+++ tests/test_pldag.py
@@ -9,72 +9,72 @@ def test_create_model():
 def test_propagate():
     model = PLDAG()
     model.set_primitives("xyz")
-    model.set_atleast("xyz", 1, alias="A")
+    a=model.set_atleast("xyz", 1)
     model.propagate()
-    assert np.array_equal(model.get("A"), np.array([complex(1j)]))
+    assert np.array_equal(model.get(a), np.array([complex(1j)]))
 
     model = PLDAG()
     model.set_primitives("xyz")
-    model.set_atleast("x", 1, alias="C")
-    model.set_atmost("yz", 1, alias="B")
-    model.set_atleast("BC", 2, alias="A")
+    c = model.set_atleast("x", 1)
+    b = model.set_atmost("yz", 1)
+    a = model.set_atleast([b,c], 2)
     model.propagate()
-    assert np.array_equal(model.get("C"), np.array([1j]))
-    assert np.array_equal(model.get("B"), np.array([1j]))
-    assert np.array_equal(model.get("A"), np.array([1j]))
+    assert np.array_equal(model.get(c), np.array([1j]))
+    assert np.array_equal(model.get(b), np.array([1j]))
+    assert np.array_equal(model.get(a), np.array([1j]))
     model.set_primitive("x", 1+1j)
     model.set_primitive("y", 0j)
     model.propagate()
-    assert np.array_equal(model.get("C"), np.array([1+1j]))
-    assert np.array_equal(model.get("B"), np.array([1+1j]))
-    assert np.array_equal(model.get("A"), np.array([1+1j]))
+    assert np.array_equal(model.get(c), np.array([1+1j]))
+    assert np.array_equal(model.get(b), np.array([1+1j]))
+    assert np.array_equal(model.get(a), np.array([1+1j]))
     
     model = PLDAG()
     model.set_primitives("xyz")
-    model.set_atmost("x", 0, alias="C")
-    model.set_atleast("xy", 1, alias="B")
-    model.set_atleast("BC", 2, alias="A")
+    c=model.set_atmost("x", 0)
+    b=model.set_atleast("xy", 1)
+    a=model.set_atleast([b, c], 2)
     model.propagate()
     assert np.array_equal(model.bounds, np.array([1j, 1j, 1j, 1j, 1j, 1j]))
     model.set_primitive("x", 1+1j)
     model.propagate()
-    assert np.array_equal(model.get("C"), np.array([0j]))
-    assert np.array_equal(model.get("A"), np.array([0j]))
+    assert np.array_equal(model.get(c), np.array([0j]))
+    assert np.array_equal(model.get(a), np.array([0j]))
 
 def test_integer_bounds():
 
     model = PLDAG()
     model.set_primitives("z", complex(0, 10000))
-    model.set_atleast("z", 3000, alias="A")
+    a=model.set_atleast("z", 3000)
     model.propagate()
     assert np.array_equal(model.get("z"), np.array([complex(0, 10000)]))
-    assert np.array_equal(model.get("A"), np.array([complex(0, 1)]))
+    assert np.array_equal(model.get(a), np.array([complex(0, 1)]))
     model.set_primitives("z", complex(3000, 10000))
     model.propagate()
-    assert np.array_equal(model.get("A"), np.array([complex(1, 1)]))
+    assert np.array_equal(model.get(a), np.array([complex(1, 1)]))
 
 
 def test_replace_composite():
     model = PLDAG()
     model.set_primitives("xyz")
-    model.set_atleast("xyz", 1, alias="A")
+    a = model.set_atleast("xyz", 1)
     model.propagate()
-    assert np.array_equal(model.get("A"), np.array([1j]))
-    model.set_atleast("xyz", 0, alias="A")
+    assert np.array_equal(model.get(a), np.array([1j]))
+    a = model.set_atleast("xyz", 0)
     model.propagate()
-    assert np.array_equal(model.get("A"), np.array([1+1j]))
+    assert np.array_equal(model.get(a), np.array([1+1j]))
 
 def test_get():
     model = PLDAG()
     model.set_primitives("xyz")
-    model.set_atleast("xyz", -1, alias="A")
-    for alias, expected in zip(["x", "y", "z", "A"], np.array([[1j], [1j], [1j], [1j]])):
-        assert np.array_equal(model.get(alias), expected)
+    a=model.set_atleast("xyz", -1)
+    for id, expected in zip(["x", "y", "z", a], np.array([[1j], [1j], [1j], [1j]])):
+        assert np.array_equal(model.get(id), expected)
 
 def test_test():
     model = PLDAG()
     model.set_primitives("xyz")
-    id = model.set_atleast("xyz", 1, alias="A")
+    id = model.set_atleast("xyz", 1)
 
     assert model.test({"x": 1+1j}).get(id) == 1+1j
     assert model.test({"x": 1j}).get(id) == 1j
@@ -91,9 +91,9 @@ def test_test():
 def test_test_second():
     model = PLDAG() 
     model.set_primitives("xyz")
-    model.set_atmost(["x"], 0, alias="C")
-    model.set_atleast(["y", "z"], 2, alias="B")
-    a = model.set_atleast(["B", "C"], 1, alias="A")
+    c = model.set_atmost(["x"], 0)
+    b = model.set_atleast(["y", "z"], 2)
+    a = model.set_atleast([b, c], 1)
     assert model.test({
         "x": 1+1j, 
         "y": 1+1j, 
@@ -117,48 +117,48 @@ def test_test_second():
 def test_dependencies():
     model = PLDAG() 
     model.set_primitives("xyz")
-    model.set_atmost(["x"], 0, alias="C")
-    model.set_atleast(["y", "z"], 2, alias="B")
-    model.set_atleast(["B", "C"], 1, alias="A")
-    assert list(chain(*model.dependencies("A").values())) == ["C", "B"]
-    assert list(chain(*model.dependencies("B").keys())) == ["y", "z"]
-    assert list(chain(*model.dependencies("C").keys())) == ["x"]
+    c=model.set_atmost(["x"], 0)
+    b=model.set_atleast(["y", "z"], 2)
+    a=model.set_atleast([b,c], 1)
+    assert model.dependencies(a) == {c,b}
+    assert model.dependencies(b) == {"y", "z"}
+    assert model.dependencies(c) == {"x"}
 
 def test_negated():
     model = PLDAG() 
     model.set_primitives("xyz")
-    model.set_atmost(["x"], 1, alias="C")
-    model.set_atmost(["y", "z"], 2, alias="B")
-    model.set_atleast("xyz", 2, alias="A")
-    assert model.negated("A") == False
-    assert model.negated("B") == True
-    assert model.negated("C") == True
+    c=model.set_atmost(["x"], 1)
+    b=model.set_atmost(["y", "z"], 2)
+    a=model.set_atleast("xyz", 2)
+    assert model.negated(a) == False
+    assert model.negated(b) == True
+    assert model.negated(c) == True
 
-    assert np.array_equal(model.get("A"), np.array([1j]))
-    assert np.array_equal(model.get("B"), np.array([1j]))
-    assert np.array_equal(model.get("C"), np.array([1j]))
+    assert np.array_equal(model.get(a), np.array([1j]))
+    assert np.array_equal(model.get(b), np.array([1j]))
+    assert np.array_equal(model.get(c), np.array([1j]))
 
 def test_delete():
     model = PLDAG() 
     model.set_primitives("xyz")
-    model.set_atmost(["x"], 0, alias="C")
-    model.set_atmost(["y", "z"], 1, alias="B")
-    model.set_atmost(["C", "B"], 2, alias="A")
+    c=model.set_atmost(["x"], 0)
+    b=model.set_atmost(["y", "z"], 1)
+    a=model.set_atmost([c, b], 2)
     model.delete("x")
     model.propagate()
     # Since x is removed, C should be able to be true for ever
     # Same as C = 0 >= 0
-    np.array_equal(model.get("C"), np.array([1+1j]))
+    np.array_equal(model.get(c), np.array([1+1j]))
     # A and B should stay the same
-    np.array_equal(model.get("B"), np.array([1j]))
-    np.array_equal(model.get("A"), np.array([1+1j]))
+    np.array_equal(model.get(b), np.array([1j]))
+    np.array_equal(model.get(a), np.array([1+1j]))
     model.delete("y")
     model.propagate()
 
 def test_cycle_detection():
     model = PLDAG() 
     model.set_primitives("xyz")
-    model.set_atleast("xyz", -1, alias="A")
+    model.set_atleast("xyz", -1)
     # There is no way using the set functions to create a cycle
     # So we need to modify the prime table
     # Here we set that "x" as "A" as input
@@ -174,19 +174,20 @@ def test_cycle_detection():
     # which is 1-1
     model.get("x")[0] == 1+1j
 
-def test_multiple_alias():
+def test_multiple_pointers():
 
     model = PLDAG()
     model.set_primitives("xyz")
-    model.set_atleast("xyz", 1, alias="A")
-    model.set_atleast("xyz", 1, alias="B")
+    a=model.set_atleast("xyz", 1)
+    b=model.set_atleast("xyz", 1)
     model.propagate()
-    assert np.array_equal(model.get("A"), np.array([1j]))
-    assert np.array_equal(model.get("B"), np.array([1j]))
+    assert np.array_equal(model.get(a), np.array([1j]))
+    assert np.array_equal(model.get(b), np.array([1j]))
 
-    model.set_atleast(["A", "B"], -1, alias="C")
-    dependencies = model.dependencies("C")
-    assert dependencies[model._amap["A"]] == dependencies[model._amap["B"]]
+    c=model.set_atleast([a, b], -1)
+    dependencies = list(model.dependencies(c))
+    assert len(dependencies) == 1
+    assert (dependencies[0] == a) and (dependencies[0] == b)
 
 def test_to_polyhedron():
 
@@ -197,8 +198,8 @@ def test_to_polyhedron():
     model.set_primitive("d", -4+5j)
     model.set_primitive("e", 1j)
     model.set_atleast("be", 3)
-    model.set_atleast("abcd", -9, alias="A")
-    model.set_atmost("abcd", 5, alias="B")
+    model.set_atleast("abcd", -9)
+    model.set_atmost("abcd", 5)
     A,b,vs = model.to_polyhedron()
     assert np.array_equal(A, np.array([
         [  0,   1,   0,   0,   1,  -3,   0,   0],
@@ -230,21 +231,21 @@ def test_to_polyhedron():
 
     model = PLDAG()
     model.set_primitives("xyz")
-    model.set_atleast("xyz", 1, alias="A")
+    a=model.set_atleast("xyz", 1)
     A,b,vs = model.to_polyhedron()
     assert np.array_equal(A, np.array([[1,1,1,-1]]))
     assert np.array_equal(b, np.array([ 0]))
     assert len(vs) == 4
 
-    A,b,vs = model.to_polyhedron(fix={"A": 1, "x": 1, "y": 1, "z": 1})
+    A,b,vs = model.to_polyhedron(fix={a: 1, "x": 1, "y": 1, "z": 1})
     assert np.array_equal(A, np.array([[1,1,1,-1], [1,1,1,1]]))
     assert np.array_equal(b, np.array([ 0, 4]))
 
-    A,b,vs = model.to_polyhedron(fix={"A": -1, "x": -1, "y": -1, "z": -1})
+    A,b,vs = model.to_polyhedron(fix={a: -1, "x": -1, "y": -1, "z": -1})
     assert np.array_equal(A, np.array([[1,1,1,-1], [1,1,1,1]]))
     assert np.array_equal(b, np.array([ 0, -4]))
 
-    A,b,vs = model.to_polyhedron(fix={"A": 2, "x": 2, "y": 0, "z": 0})
+    A,b,vs = model.to_polyhedron(fix={a: 2, "x": 2, "y": 0, "z": 0})
     assert np.array_equal(A, np.array([[1,1,1,-1], [1,0,0,1], [0,1,1,0]]))
     assert np.array_equal(b, np.array([ 0, 4, 0]))
 
